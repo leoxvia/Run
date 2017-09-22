b0503f108e6196f0a39ad69ea3a13c62a031fdcc
林俊清 <junqing.lin@ehang.com>
2017-09-22 17:49:34 +0800

    改进 pull push。

---
+++ pull.bat
@@ -0,0 +1,27 @@
+cd %userprofile%\Documents\Projects\VS\Windows
+cd EHang.FormationCreator
+git push usb
+cd ..
+cd EHang.FormationCreator2
+git push usb
+cd ..
+cd EHang.Libraries
+git push usb
+cd ..
+cd EHang.OperationPanelOfThatAircraft
+git push usb
+cd ..
+cd EHang.Sdk3
+git push usb
+cd ..
+cd EHang.Sdk4
+git push usb
+cd ..
+cd EHang.UI
+git push usb
+cd ..
+cd EHang.UserToolkit
+git push usb
+cd %userprofile%\Documents\FormationData
+git push usb
+pause
---
+++ push.bat
@@ -0,0 +1,27 @@
+cd %userprofile%\Documents\Projects\VS\Windows
+cd EHang.FormationCreator
+git pull usb
+cd ..
+cd EHang.FormationCreator2
+git pull usb
+cd ..
+cd EHang.Libraries
+git pull usb
+cd ..
+cd EHang.OperationPanelOfThatAircraft
+git pull usb
+cd ..
+cd EHang.Sdk3
+git pull usb
+cd ..
+cd EHang.Sdk4
+git pull usb
+cd ..
+cd EHang.UI
+git pull usb
+cd ..
+cd EHang.UserToolkit
+git pull usb
+cd %userprofile%\Documents\FormationData
+git pull usb
+pause
