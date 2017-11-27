bd6f5bfda61f196ae230120c100506a8449699b4
林俊清 <junqing.lin@ehang.com>
2017-11-27 17:58:52 +0800

    修改 pull

--- pull.bat
+++ pull.bat
@@ -1,27 +1,27 @@
 cd %userprofile%\Documents\Projects\VS\Windows
 cd EHang.FormationCreator
-git pull usb
+git pull usb dev
 cd ..
 cd EHang.FormationCreator2
-git pull usb
+git pull usb master
 cd ..
 cd EHang.Libraries
-git pull usb
+git pull usb dev
 cd ..
 cd EHang.OperationPanelOfThatAircraft
-git pull usb
+git pull usb dev
 cd ..
 cd EHang.Sdk3
-git pull usb
+git pull usb dev-new-firmware-formation
 cd ..
 cd EHang.Sdk4
-git pull usb
+git pull usb master
 cd ..
 cd EHang.UI
-git pull usb
+git pull usb dev
 cd ..
 cd EHang.UserToolkit
-git pull usb
+git pull usb dev
 cd %userprofile%\Documents\FormationData
-git pull usb
+git pull usb master
 pause
